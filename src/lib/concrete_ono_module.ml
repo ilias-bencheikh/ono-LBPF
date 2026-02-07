2485d9a0113f7dd7eda47f7f64c00333cd8276e2
Jules Péris <jules.peris2801@gmail.com>
2026-02-07 16:40:17 +0100

    Implementation de random_i32.wat + code ocaml pour concrete random_i32

--- src/lib/concrete_ono_module.ml
+++ src/lib/concrete_ono_module.ml
@@ -9,12 +9,18 @@ let print_i64 (n : Kdo.Concrete.I64.t) : (unit, _) Result.t =
   Ok ()
 
 
+let random_i32  (_ : unit) : (Kdo.Concrete.I32.t,_) Result.t = 
+  let randint = Random.int32 Int32.max_int in 
+  Ok (Kdo.Concrete.I32.of_int32 randint)
+
+
 let m =
   let open Kdo.Concrete.Extern_func in
   let open Kdo.Concrete.Extern_func.Syntax in
   let functions = [ 
     ("print_i32", Extern_func (i32 ^->. unit, print_i32));
-    ("print_i64", Extern_func (i64 ^->. unit, print_i64))
+    ("print_i64", Extern_func (i64 ^->. unit, print_i64));
+    ("random_i32", Extern_func (unit ^->. i32 , random_i32))
     ] in
   {
     Kdo.Extern.Module.functions;
